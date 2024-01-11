a316f66f18f3f9bd9cf835943b3cd58dd56d1c81
sooraj-srini <cs20b075@smail.iitm.ac.in>
2024-01-11 18:14:59 +0530

    Optimized Bitset.use_range and Bitset.free_range

--- src/bitset.ml
+++ src/bitset.ml
@@ -74,14 +74,44 @@ module Make (B : Context.A_DISK) = struct
     let* leaf = get_leaf t i in
     free_leaf leaf (i mod bit_size)
 
-  (* TODO: Optimize free_range to use less set_uint calls *)
-  let rec free_range t (id, len) =
-    if len = 0
-    then Lwt_result.return ()
-    else (
-      let int_id = Int64.to_int (B.Id.to_int64 id) in
-      let* () = free t int_id in
-      free_range t (B.Id.succ id, len - 1))
+  let free_range_leaf t (ind, len) =
+    assert (ind / 8 = (ind + len - 1) / 8) ;
+    let rec set_used cur_ind value =
+      if cur_ind = ind + len
+      then value
+      else (
+        let offset = cur_ind mod 8 in
+        let flag = value land (1 lsl offset) in
+        assert (flag > 0) ;
+        let update = value lxor (1 lsl offset) in
+        set_used (cur_ind + 1) update)
+    in
+    let pos = ind / 8 in
+    let* value = Sector.get_uint8 t pos in
+    let update = set_used ind value in
+    Sector.set_uint8 t pos update
+
+  let free_range t (id, len) =
+    let page_size = get_page_size () in
+    let bit_size = page_size * 8 in
+    let rec split leaf (ind, len) =
+      match len with
+      | 0 -> Lwt_result.return ()
+      | len ->
+        let cur_len = min len (8 - (ind mod 8)) in
+        let next_ind = ind + cur_len in
+        let next_len = len - cur_len in
+        let* next_leaf =
+          if ind / bit_size <> next_ind / bit_size
+          then get_leaf t next_ind
+          else Lwt_result.return leaf
+        in
+        let* () = free_range_leaf leaf (ind mod bit_size, cur_len) in
+        split next_leaf (next_ind, next_len)
+    in
+    let ind = Int64.to_int @@ B.Id.to_int64 id in
+    let* leaf = get_leaf t ind in
+    split leaf (ind, len)
 
   let use_leaf t i =
     let pos = i / 8 in
@@ -98,14 +128,44 @@ module Make (B : Context.A_DISK) = struct
     let* leaf = get_leaf t i in
     use_leaf leaf (i mod bit_size)
 
-  (* TODO: Optimize use_range to use less set_uint calls *)
-  let rec use_range t (id, len) =
-    if len = 0
-    then Lwt_result.return ()
-    else (
-      let int_id = Int64.to_int (B.Id.to_int64 id) in
-      let* () = use t int_id in
-      use_range t (B.Id.succ id, len - 1))
+  let use_range_leaf t (ind, len) =
+    assert (ind / 8 = (ind + len - 1) / 8) ;
+    let rec set_used cur_ind value =
+      if cur_ind = ind + len
+      then value
+      else (
+        let offset = cur_ind mod 8 in
+        let flag = value land (1 lsl offset) in
+        assert (flag = 0) ;
+        let update = value lor (1 lsl offset) in
+        set_used (cur_ind + 1) update)
+    in
+    let pos = ind / 8 in
+    let* value = Sector.get_uint8 t pos in
+    let update = set_used ind value in
+    Sector.set_uint8 t pos update
+
+  let use_range t (id, len) =
+    let page_size = get_page_size () in
+    let bit_size = page_size * 8 in
+    let rec split leaf (ind, len) =
+      match len with
+      | 0 -> Lwt_result.return ()
+      | len ->
+        let cur_len = min len (8 - (ind mod 8)) in
+        let next_ind = ind + cur_len in
+        let next_len = len - cur_len in
+        let* next_leaf =
+          if ind / bit_size <> next_ind / bit_size
+          then get_leaf t next_ind
+          else Lwt_result.return leaf
+        in
+        let* () = use_range_leaf leaf (ind mod bit_size, cur_len) in
+        split next_leaf (next_ind, next_len)
+    in
+    let ind = Int64.to_int @@ B.Id.to_int64 id in
+    let* leaf = get_leaf t ind in
+    split leaf (ind, len)
 
   let create_leaf () =
     let* t = Sector.create () in
